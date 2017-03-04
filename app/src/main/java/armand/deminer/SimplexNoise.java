aa437180680b85cdee86ba6a9a161e394f0e1577
Petru (Armand) Bancila <petru.bancila@kcl.ac.uk>
2017-03-04 01:16:55 +0000

    Add author info for algo
    I now mention who wrote the simplex noise generation algorithm. I also refactored some of the code.

--- app/src/main/java/armand/deminer/SimplexNoise.java
+++ app/src/main/java/armand/deminer/SimplexNoise.java
@@ -146,3 +146,20 @@ class SimplexNoise {  // Simplex noise in 2D, 3D and 4D
         }
     }
 }
+
+/*
+ * A speed-improved simplex noise algorithm for 2D, 3D and 4D in Java.
+ *
+ * Based on example code by Stefan Gustavson (stegu@itn.liu.se).
+ * Optimisations by Peter Eastman (peastman@drizzle.stanford.edu).
+ * Better rank ordering method for 4D by Stefan Gustavson in 2012.
+ *
+ * This could be speeded up even further, but it's useful as it is.
+ *
+ * Version 2012-03-09
+ *
+ * This code was placed in the public domain by its original author,
+ * Stefan Gustavson. You may use it as you see fit, but
+ * attribution is appreciated.
+ *
+ */
